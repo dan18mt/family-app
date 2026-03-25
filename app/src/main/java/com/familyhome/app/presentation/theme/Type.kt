b9298a428cb92084c9ea37461774c5531c2d0b0e
Wildan.Tahtadi <wild0597@gmail.com>
2026-03-25 09:40:40 +0700

    Fix release

--- app/src/main/java/com/familyhome/app/presentation/theme/Type.kt
+++ app/src/main/java/com/familyhome/app/presentation/theme/Type.kt
@@ -7,12 +7,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
-import com.familyhome.app.R
-
 private val provider = GoogleFont.Provider(
     providerAuthority = "com.google.android.gms.fonts",
     providerPackage   = "com.google.android.gms",
-    certificates      = R.array.com_google_android_gms_fonts_certs,
+    certificates      = emptyList<List<ByteArray>>(),
 )
 
 private val nunitoFont = GoogleFont("Nunito")
